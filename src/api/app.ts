cb9a76a76a85925c625b02191dd191344d79d237
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-12-22 10:01:11 -0400

    Update app.ts

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,8 +1,51 @@
 import fastify from 'fastify';
+import fastifySensible from 'fastify-sensible';
+
+import envSchema from 'env-schema';
+import {MongoClient} from 'mongodb';
+
+import {TaskRepository} from '../pkg/task/repository';
+import {TaskService} from '../pkg/task/service';
+import {TaskRouter} from './routes/task';
 
 async function main() {
-  const app = fastify();
-  await app.listen(8080);
+  const config = getConfig();
+  const {database} = await initDB(config.MONGO_URL);
+
+  const app = fastify().register(fastifySensible);
+
+  const taskRepository = new TaskRepository(database.collection('task'));
+  const taskService = new TaskService(taskRepository);
+  TaskRouter(app, taskService);
+
+  await app.listen(config.PORT, config.HOST);
+}
+
+function getConfig() {
+  return envSchema({
+    schema: {
+      type: 'object',
+      required: ['PORT', 'HOST', 'MONGO_URL'],
+      properties: {
+        PORT: {type: 'integer', default: 3000},
+        HOST: {type: 'string', default: '0.0.0.0'},
+        MONGO_URL: {type: 'string'},
+      },
+    },
+    dotenv: true,
+  }) as {
+    PORT: number;
+    HOST: string;
+    MONGO_URL: string;
+  };
+}
+
+async function initDB(url: string) {
+  const client = await MongoClient.connect(url);
+  return {
+    client,
+    database: client.db(),
+  };
 }
 
 main();
