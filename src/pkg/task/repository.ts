e67de6b705f2f37ae6b2ff7320437c0b2a582e4f
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-12-22 09:59:22 -0400

    Create Task Pagination

--- src/pkg/task/repository.ts
+++ src/pkg/task/repository.ts
@@ -1,8 +1,10 @@
-import {Collection} from 'mongodb';
+import {Collection, Filter, FindOptions} from 'mongodb';
 import {Task} from '../entities/task';
 
 export interface ITaskRepository {
+  countTask(): Promise<number>;
   createTask(task: Task): Promise<boolean>;
+  findTaskPage(page: number, limit: number): Promise<Task[]>;
 }
 
 export class TaskRepository implements ITaskRepository {
@@ -12,9 +14,27 @@ export class TaskRepository implements ITaskRepository {
     this.collection = collection;
   }
 
+  public async countTask(): Promise<number> {
+    return this.collection.countDocuments();
+  }
+
   public async createTask(task: Task): Promise<boolean> {
     const {name, description} = task;
     const insert = await this.collection.insertOne({name, description});
     return insert.acknowledged;
+  }
+
+  public async findTaskPage(page: number, limit: number): Promise<Task[]> {
+    const filter: Filter<Task> = {};
+    const options: FindOptions<Task> = {
+      projection: {
+        _id: 1,
+        name: 1,
+        description: 1,
+      },
+      skip: (page - 1) * limit,
+      limit: limit,
+    };
+    return this.collection.find<Task>(filter, options).toArray();
   }
 }
--- src/pkg/task/service.ts
+++ src/pkg/task/service.ts
@@ -1,8 +1,19 @@
 import {Task} from '../entities/task';
 import {TaskRepository} from './repository';
 
+export interface TaskPage {
+  pageInfo: {
+    total: number;
+    perPage: number;
+    currentPage: number;
+    hasNextPage: boolean;
+  };
+  tasks: Task[];
+}
+
 export interface ITaskService {
   insertTask(task: Task): Promise<boolean>;
+  getTaskPage(page: number, limit: number): Promise<TaskPage>;
 }
 
 export class TaskService implements ITaskService {
@@ -14,5 +25,18 @@ export class TaskService implements ITaskService {
 
   public insertTask(task: Task): Promise<boolean> {
     return this.repository.createTask(task);
+  }
+
+  public async getTaskPage(page: number, limit: number): Promise<TaskPage> {
+    const total = await this.repository.countTask();
+    return {
+      pageInfo: {
+        total,
+        perPage: limit,
+        currentPage: page,
+        hasNextPage: Math.ceil(total / limit) > page,
+      },
+      tasks: await this.repository.findTaskPage(page, limit),
+    };
   }
 }
